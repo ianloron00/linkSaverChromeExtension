8a320435374b21386f7981484199d8f8adccf428
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-03-28 23:11:44 -0300

    chrome extension finished!

--- index.js
+++ index.js
@@ -2,67 +2,64 @@
 
 const inputBtn = document.getElementById("input-btn")
 const delBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-web")
-let myLeads = []
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+let myLeads = []
 
-
-renderStoredLeads()
-
-
-function renderStoredLeads() {
-    console.log(localStorage)
-    console.log(leadsFromLocalStorage)
-    
-    if (leadsFromLocalStorage) {
-        myLeads = leadsFromLocalStorage
-        renderLeads()
-    }
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    // addListItem()
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
-})
-
-delBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    renderLeads()
-})
-
-function addListItem() {
-    ulEl.innerHTML += `
-        <li>
-            <a href=${myLeads[myLeads.length - 1]} target='_blank'> 
-                ${myLeads[myLeads.length - 1]}
-            </a>
-        </li>
-    `
-    inputEl.value = ""
-}
-
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
     // better to do all rendering at once because it is costy!
     ulEl.innerHTML = listItems
 }
 
+function addListItem(lead) {
+    ulEl.innerHTML += `
+        <li>
+            <a href=${lead} target='_blank'> 
+                ${lead}
+            </a>
+        </li>
+    `
+}
 
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true},
+        function(tabs) {
+            let activeTab = tabs[0]
+            let activeTabId = activeTab.id
+        })
 
+    myLeads.push(tabs[0].url)
+    localStorage.getItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
 
-// for (let i = 0; i < myLeads.length; i++) {
-    // one way
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // second way
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-// }
+delBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    // render(myLeads)
+    addListItem(myLeads[myLeads.length-1])
+})
